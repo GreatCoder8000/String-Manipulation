90d8bfbdc952a17f2bc71b7156912d8f81cd261f
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-14 11:53:55 +0000

    Create strings01a.py

---
+++ strings01a.py
@@ -0,0 +1,14 @@
+string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print(string)
+
+print("hello")
+
+print(len("hello"))
+
+print(len(string))
+
+print("the length of the string - "+string+" - is "+len(string))
+
+userstring = input("input string ")
+print("the length of the string - "+userstring+" - is "+len(userstring))
