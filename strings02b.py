a28263918cc8a7ea5fe2191af9d71f206d6fcbb2
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-14 11:49:02 +0000

    Create strings02b.py

---
+++ strings02b.py
@@ -0,0 +1,22 @@
+string = "A"
+
+print(string)
+
+if len(string) == 1:# check for one chracter
+	print("the string is one character long")
+else:
+	print("the string is is not one character long")
+
+string = "A":
+
+if len(string) > 1:# check for more than one character
+	print("the string is over a single character")
+else:
+	print("the string is 1 or 0 characters")
+
+if len(string) > 1:# check for more than one character
+	print("the string is over a single character")
+elif len(string) == 1:# check for one chracter
+	print("the string is a single character")
+else:
+	print("the string is 1 or 0 characters")
