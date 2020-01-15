c0d4cb8778eaa9ee6479df104e6e07c42010f633
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-15 13:56:42 +0000

    Update strings02b.py

--- strings02b.py
+++ strings02b.py
@@ -16,5 +16,13 @@ start_position = position
 uinput = input("string")
 find = input("what to find")
 
-while position <= len(uinput)):
-  position = input.find(find,
+while position >= 0:
+  position = input.find(find,start_position)
+  
+  if position != -1:
+    print("found at position:",position)
+    start_position = position
+    count+=1
+  else:
+    print("no more matches")
+print("found",count,"of",find,"in phrase:",uinput)
