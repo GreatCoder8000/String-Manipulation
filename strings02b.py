6d41046e24282cc646e2e775a0d598a70ce6dec6
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-15 13:59:06 +0000

    Update strings02b.py

--- strings02b.py
+++ strings02b.py
@@ -7,8 +7,8 @@ print(".. second 'an' appears at position: ", string.find("an",15))
 first_exam = string.find("matching")
 second_exam = string.find("matching",first_exam+1)
 
-print(".. first "exam" appears at position: ", first_exam)
-print(".. second "exam" appears at position: ", second_exam)
+print(".. first 'exam' appears at position: ", first_exam)
+print(".. second 'exam' appears at position: ", second_exam)
 
 position = 0
 count = 0
