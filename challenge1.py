c4f966e329ac427831dfda15ba0eaea0166e22ee
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-17 09:33:05 +0000

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,1 +1,8 @@
+proverb = "A picture's worth a thousand words"
+proverbImage = "A image is worth a thousand words"
 
+firstO = string.find("o")
+
+proverbUpper = proverb.upper()
+
+length = len(proverb)
