df8e3b1e857f8e00b44e6b681c80fd3ac02d90f7
GreatCoder8000 <44799104+GreatCoder8000@users.noreply.github.com>
2020-01-17 09:20:03 +0000

    not a clue

--- strings03b.py
+++ strings03b.py
@@ -1,1 +1,25 @@
+string = "Question: Choose from A, B or C, Answer: is C"
 
+print(string)
+
+if string[0:7] == "Question":
+  print(string, "is a Question")
+else:
+  print(string, "is NOT a Question")
+ 
+
+print(string)
+
+if string.find("answer") != -1:
+  print(string, "is a Question")
+else:
+  print(string, "is NOT a Question, so is an ", string[0:5])
+  
+string = "Answer: is C"
+
+print(string)
+
+if string[0:7] == "Answer":
+  print(string, "Question is: ",string[1,string.find("C")])
+else:
+  print(string, "is NOT a Question, so is an ", string[0:5])
